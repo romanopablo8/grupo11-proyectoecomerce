3919ec58d1803bfc21b80d57ebb8924a80265347
romanopablo83@gmail.com <romanopablo83@gmail.com>
2022-05-18 16:35:12 -0300

    subiendo esqueleto y express

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/index', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.get('/productDetail', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.get('/productCart', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.get('/register', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.get('/login', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
