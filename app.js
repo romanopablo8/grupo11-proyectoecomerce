55fb867f0df28f731c18c7d8182230772ccef4f8
Gonfurion <gorza86@gmail.com>
2022-05-20 02:52:24 -0300

    app con direcciones

--- app.js
+++ app.js
@@ -3,30 +3,26 @@ const app = express()
 const path = path()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/index', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.get('/productDetail', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.get('/productCart', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.get('/register', (req, res) => {
-    res.send('Hello World!')
-  })
-
-app.get('/login', (req, res) => {
-    res.send('Hello World!')
-  })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/login', (req,res)=>{
+  res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/register', (req,res)=>{
+  res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/productCart', (req,res)=>{
+  res.sendFile(__dirname + '/views/productCart.html');
+});
+app.get('/productDetail', (req,res)=>{
+  res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+
