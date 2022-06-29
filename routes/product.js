553384c8ba5ca94fa731422f97b8dd48a4438643
Gonfurion <gorza86@gmail.com>
2022-06-28 22:07:55 -0300

    asd

--- routes/product.js
+++ routes/product.js
@@ -4,10 +4,7 @@ const router  = express.Router();
 const controller = require( '../controllers/products' );
 
 /* GET products cart / details page. */
-<<<<<<< HEAD
 
-=======
->>>>>>> d1839f37c1d242572dfb4fde6daf5507a6861055
 router.get( '/product', controller.product);
 router.get( '/productcart', controller.productcart);
 router.get( '/productdetail', controller.productdetail);
