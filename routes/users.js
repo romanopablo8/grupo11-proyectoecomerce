1d7479a16d6ade12860bc5cd864d8ab7ffedcf19
Gonfurion <gorza86@gmail.com>
2022-06-30 22:51:53 -0300

    middlewares, register invitado, file upload en ruta

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router  = express.Router();
-
+const multer = require('multer')
+const path = require('path')
+const diskStorage = require ('../middlewares/diskStorage')
 const controller = require( '../controllers/user' );
+
+
+let fileUpload = multer({storage: diskStorage});
+let invitadoMiddleware = require('../middlewares/invitadoMiddleware');
 
 // /* GET LOGIN/REGISTER page. */
 router.get( '/login', controller.login);
 router.post('/login', controller.processlogin)
-router.get( '/register', controller.register);
-router.post( '/register', controller.register);
+router.get( '/register', invitadoMiddleware, controller.register);
+router.post( '/register', fileUpload.single('avatar'), controller.register);
 module.exports = router;
