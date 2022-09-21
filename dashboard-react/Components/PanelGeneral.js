a87ad5dd5dbf05fc4c5e523914f083062fa6ab88
romanopablo8 <romanopablo83@gmail.com>
2022-09-20 21:48:00 -0300

    componentes + css

--- dashboard-react/Components/PanelGeneral.js
+++ dashboard-react/Components/PanelGeneral.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import 
 
 function PanelGeneral () {
   return (
+    <div className='contenedor-principal'>
+
+        <div className='total-productos'>
+
+            <div className='total-usuarios'>
+
+                <div className='total-categorias'>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
   );
 }
 
--- dashboard-react/src/App.js
+++ dashboard-react/src/App.js
@@ -1,4 +1,9 @@
 import './App.css';
+import '../src/assets/componentes.css';
+import ListadoProductos from './componentes/ListadoProductos';
+import PanelCategorias from './componentes/PanelCategorias';
+import PanelDetalle from './componentes/PanelDetalle';
+import PanelGeneral from './componentes/PanelGeneral';
 
 function App() {
   return (
