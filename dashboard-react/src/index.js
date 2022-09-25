c0b14d66b440673637378e7a56d5fa026b6173e4
jacabrales911 <jcabralesg@gmail.com>
2022-09-25 15:25:00 -0500

    actualiza

--- dashboard-react/src/index.js
+++ dashboard-react/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import App from './Components/App';
+import App from './components/App';
 import './assets/css/app.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
